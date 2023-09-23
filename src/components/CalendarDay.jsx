b79f9ebdba3d8b2432c5cd697c6a64a75c27433d
Kevry Paulino <KevryP@gmail.com>
2023-09-22 20:39:09 -0400

    Added drag to highlight multiple days at once

--- src/components/CalendarDay.jsx
+++ src/components/CalendarDay.jsx
@@ -2,15 +2,35 @@ import { useEffect, useState } from "react"
 
 function CalendarDay({day}){
     const [bgColor, setBgColor] = useState("grey");
+    const [mouseDown, setMouseDown] = useState(false);
     const dayClick = color => {
         if (bgColor == "grey"){
             setBgColor("red");
         }
         else {setBgColor("grey");}
-}
-    
+    }
+    const onMouseMouve = (e) => {
+        if(mouseDown == true){
+            setBgColor("blue");
+        }
+    }
+
+    useEffect(() => {
+        addEventListener("mousedown", handleMouseDown);
+        addEventListener("mouseup", handleMouseUp);
+
+    })
+
+    const handleMouseDown = (e) => {
+        setMouseDown(true);
+    }
+    const handleMouseUp = (e) => {
+        setMouseDown(false);
+    }
+
+    //onClick={() => dayClick("red")}
     return(
-    <div className="day" onClick={() => dayClick("red")} style={{backgroundColor: bgColor}}>{day}</div>
+    <div className="day" onClick={() => dayClick("placeholder")} style={{backgroundColor: bgColor}} onMouseMove={onMouseMouve}>{day}</div>
     )
 }
 
