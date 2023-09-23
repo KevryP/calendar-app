f1cf20912f7acaaf1f01f1acd88d1c031b3c5813
Kevry Paulino <KevryP@gmail.com>
2023-09-23 16:46:08 -0400

    added early stages of drag select

--- src/components/CalendarDay.jsx
+++ src/components/CalendarDay.jsx
@@ -1,36 +1,58 @@
 import { useEffect, useState } from "react"
 
-function CalendarDay({day}){
+function CalendarDay({day, daysSelected}){
     const [bgColor, setBgColor] = useState("grey");
     const [mouseDown, setMouseDown] = useState(false);
-    const dayClick = color => {
+    let isSelected = false;
+        for(let i = daysSelected[0]; i <= daysSelected[daysSelected.length-1]; i++)
+        {
+            if (i == day){
+                isSelected = true;
+            }
+        }
+    const changeColor = color => {
         if (bgColor == "grey"){
             setBgColor("red");
+            daysSelected.push(day);
         }
-        else {setBgColor("grey");}
+        else {
+            setBgColor("grey");
+        }
     }
     const onMouseMouve = (e) => {
         if(mouseDown == true){
-            setBgColor("blue");
+            for(let i = daysSelected[0]; i <= daysSelected[daysSelected.length-1]; i++)
+            {
+                if (i == day){
+                    setBgColor("blue"); //Using this as a forced render
+                    isSelected = true;
+                }
+            }
+        }
+    }
+    const dragSelect = () => {
+        if(mouseDown == true && daysSelected.indexOf(day) === -1 ){
+            daysSelected.push(day);
         }
     }
 
     useEffect(() => {
         addEventListener("mousedown", handleMouseDown);
         addEventListener("mouseup", handleMouseUp);
-
+        addEventListener("mousemove", handleMouseMove);
     })
 
-    const handleMouseDown = (e) => {
+    const handleMouseDown = () => {
         setMouseDown(true);
     }
-    const handleMouseUp = (e) => {
+    const handleMouseUp = () => {
         setMouseDown(false);
     }
-
-    //onClick={() => dayClick("red")}
+    const handleMouseMove = () => {
+        onMouseMouve();
+    }
     return(
-    <div className="day" onClick={() => dayClick("placeholder")} style={{backgroundColor: bgColor}} onMouseMove={onMouseMouve}>{day}</div>
+    <div className="day" onClick={() => changeColor("placeholder")} style={{backgroundColor: isSelected ? "pink" : bgColor}} onMouseMove={() => dragSelect() }>{day}</div>
     )
 }
 
--- src/components/CalendarDays.jsx
+++ src/components/CalendarDays.jsx
@@ -3,15 +3,27 @@ import Day from "./CalendarDay"
 
 function CalendarDays() {
     let days = [];
-    for(let i=0; i< 42; i++){
+    let daysSelected = [];
+    for(let i=0; i < 42; i++){
         days.push(i);
+    }
+    function selectDays () {
+        daysSelected.sort((a,b) => a - b);
+        let largestDay = daysSelected[daysSelected.length-1]
+        for(let i = daysSelected[0] + 1; i < largestDay; i++){
+            daysSelected.push(i);
+        }
+
     }
 
     return(
         <div className="calendar-body">
             {days.map((day) => {
             return(
-                <Day day={day}></Day>
+                <>
+                <Day day={day} daysSelected={daysSelected}></Day>
+                <button onClick={() => selectDays()}></button>
+                </>
             )
         })}
         </div>
